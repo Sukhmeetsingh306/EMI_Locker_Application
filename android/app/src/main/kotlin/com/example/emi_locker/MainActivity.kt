d665412869878721b7761e36e67bec09f1f9d81c
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2026-03-10 01:27:01 +0530

    feat: added dervices and recievers from the koylin iles

--- android/app/src/main/kotlin/com/example/emi_locker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/emi_locker/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.emi_locker
 
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
+import android.os.Bundle
+import android.os.UserManager
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -7,6 +12,30 @@ import io.flutter.plugin.common.MethodChannel
 class MainActivity : FlutterActivity() {
 
     private val CHANNEL = "emi/kiosk"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val dpm = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        val component = ComponentName(this, EmiDeviceAdminReceiver::class.java)
+
+        if (dpm.isDeviceOwnerApp(packageName)) {
+
+            // Allow only this app for kiosk mode
+            dpm.setLockTaskPackages(component, arrayOf(packageName))
+
+            // Prevent Safe Mode boot
+            dpm.addUserRestriction(component, UserManager.DISALLOW_SAFE_BOOT)
+
+            // Optional security restrictions
+            dpm.addUserRestriction(component, UserManager.DISALLOW_FACTORY_RESET)
+
+            dpm.addUserRestriction(component, UserManager.DISALLOW_ADD_USER)
+
+            // Disable status bar (prevents pull-down settings)
+            dpm.setStatusBarDisabled(component, true)
+        }
+    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
