0d09bc710b6eebe1531066c4957171f59aaff0cd
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2026-03-09 19:11:12 +0530

    fix: simplify EmiPaymentModel by removing unused fields and updating JSON parsing

--- lib/models/emi_payment_model.dart
+++ lib/models/emi_payment_model.dart
@@ -1,31 +1,24 @@
 class EmiPaymentModel {
+  final String id;
   final int installmentNumber;
   final DateTime dueDate;
-  final DateTime? paidDate;
   final double amount;
-  final double? percentage;
   final String status;
 
   EmiPaymentModel({
+    required this.id,
     required this.installmentNumber,
     required this.dueDate,
-    this.paidDate,
     required this.amount,
-    this.percentage,
     required this.status,
   });
 
   factory EmiPaymentModel.fromJson(Map<String, dynamic> json) {
     return EmiPaymentModel(
-      installmentNumber: json["installmentNumber"] ?? 0,
+      id: json["_id"],
+      installmentNumber: json["installmentNumber"],
       dueDate: DateTime.parse(json["dueDate"]),
-      paidDate: json["paidDate"] != null
-          ? DateTime.parse(json["paidDate"])
-          : null,
       amount: (json["amount"] ?? 0).toDouble(),
-      percentage: json["percentage"] != null
-          ? (json["percentage"]).toDouble()
-          : null,
       status: json["status"] ?? "pending",
     );
   }
