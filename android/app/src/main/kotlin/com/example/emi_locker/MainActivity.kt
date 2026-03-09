5b232801b51a1222f20fd1e9fce6f7a835b62dec
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2026-03-09 13:47:36 +0530

    feat: implement device lock functionality in MainActivity on resume

--- android/app/src/main/kotlin/com/example/emi_locker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/emi_locker/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.example.emi_locker
 
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    override fun onResume() {
+        super.onResume()
+
+        val dpm = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        val componentName = ComponentName(this, EmiDeviceAdminReceiver::class.java)
+
+        if (dpm.isDeviceOwnerApp(packageName)) {
+            startLockTask()
+        }
+    }
+}
