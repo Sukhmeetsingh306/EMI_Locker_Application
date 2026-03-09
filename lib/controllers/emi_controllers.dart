68d1450f3f67ac4c8a51b4b1e79e89e480839c6c
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2026-03-09 12:32:38 +0530

    feat: implement EmiController to manage EMI data retrieval and payment schedules

---
+++ lib/controllers/emi_controllers.dart
@@ -0,0 +1,35 @@
+import '../core/api/api_clients.dart';
+import '../models/emi_payment_model.dart';
+import '../models/emi_model.dart';
+
+class EmiController {
+  final ApiClient apiClient = ApiClient();
+
+  /// Get all EMIs for logged user
+  Future<List<EmiModel>> getMyEmis() async {
+    try {
+      final response = await apiClient.dio.get("/emis/my");
+
+      final List data = response.data["data"];
+
+      return data.map((e) => EmiModel.fromJson(e)).toList();
+    } catch (e) {
+      print("EMI fetch error: $e");
+      return [];
+    }
+  }
+
+  /// Get payment schedule
+  Future<List<EmiPaymentModel>> getEmiPayments(String emiId) async {
+    try {
+      final response = await apiClient.dio.get("/emis/$emiId/payments");
+
+      final List data = response.data["data"];
+
+      return data.map((e) => EmiPaymentModel.fromJson(e)).toList();
+    } catch (e) {
+      print("EMI payment fetch error: $e");
+      return [];
+    }
+  }
+}
