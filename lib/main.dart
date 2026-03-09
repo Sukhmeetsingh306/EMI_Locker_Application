2c6b41f3ba6c2cbbd405edc10bba52123d749081
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2026-03-10 01:28:43 +0530

    feat: made that it will fetch informatoin about due after every 15 minute from work manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:workmanager/workmanager.dart';
+import 'package:dio/dio.dart';
 
 import 'router/app_router.dart' as AppRouter;
 
+const String lockCheckTask = "lockCheckTask";
+
+/// Background worker
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    if (task == lockCheckTask) {
+      try {
+        final dio = Dio();
+
+        final response = await dio.get(
+          "http://YOUR_SERVER_IP:1600/api/device-lock/me",
+        );
+
+        final locked = response.data["data"]["data"]["deviceLocked"];
+
+        if (locked == true) {
+          print("Device should be locked");
+
+          // Later we trigger kiosk + lock screen
+        }
+      } catch (e) {
+        print("Background lock check failed: $e");
+      }
+    }
+
+    return Future.value(true);
+  });
+}
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// Initialize WorkManager
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+
+  /// Run every 15 minutes
+  Workmanager().registerPeriodicTask(
+    "1",
+    lockCheckTask,
+    frequency: const Duration(minutes: 15),
+  );
 
   runApp(const EmiLockerApp());
 }
