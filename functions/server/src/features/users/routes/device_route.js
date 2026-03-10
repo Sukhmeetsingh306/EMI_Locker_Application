2e3185ba82376d54de20de5845d1c0ef8ff63d30
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2026-03-10 20:53:11 +0530

    feat: made device to store the device id

--- functions/server/src/features/users/routes/device_route.js
+++ functions/server/src/features/users/routes/device_route.js
@@ -3,23 +3,21 @@ const router = express.Router();
 const UserDevice = require("../models/userDevice.model");
 
 router.post("/register-device", async (req, res) => {
-  try {
-    const { deviceId, model, osVersion, fcmToken } = req.body;
-
-    const device = await UserDevice.create({
-      deviceId,
-      model,
-      osVersion,
-      fcmToken,
-      deviceLocked: false
-    });
-
-    res.json({
-      success: true,
-      message: "Device registered",
-      data: device
-    });
-
+    try {
+        const { androidId, model, osVersion, fcmToken } = req.body;
+        let device = await UserDevice.findOne({ androidId });
+        if (!device) {
+            device = await UserDevice.create({
+            androidId,
+            model,
+            osVersion,
+            fcmToken
+            });
+        }
+        res.json({
+            success: true,
+            device
+        });
   } catch (err) {
     res.status(500).json({ success:false, message: err.message });
   }
